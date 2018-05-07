b0b6ec12f4ce2b31d5f3819942465d844399f123
Angeliz <471330418@qq.com>
2018-05-07 22:11:07 +0800

    add、

---
+++ webapp/src/api/index.js
@@ -0,0 +1,67 @@
+// 配置API接口地址
+var root='http://www.freekg.cn/poet'
+// var root='/poet/datasource'
+// 引用axios
+var axios = require('axios')
+// 自定义判断元素类型JS
+function toType (obj) {
+  return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
+}
+// 参数过滤函数
+function filterNull (o) {
+  for (var key in o) {
+    if (o[key] === null) {
+      delete o[key]
+    }
+    if (toType(o[key]) === 'string') {
+      o[key] = o[key].trim()
+    } else if (toType(o[key]) === 'object') {
+      o[key] = filterNull(o[key])
+    } else if (toType(o[key]) === 'array') {
+      o[key] = filterNull(o[key])
+    }
+  }
+  return o
+}
+/*
+  参考说明文档地址：
+  https://cnodejs.org/topic/5378720ed6e2d16149fa16bd
+*/
+
+function apiAxios (method, url, params, success, failure) {
+  if (params) {
+    params = filterNull(params)
+  }
+  axios({
+    method: method,
+    url: url,
+    data: method === 'POST' || method === 'PUT' ? params : null,
+    params: method === 'GET' || method === 'DELETE' ? params : null,
+    baseURL: root,
+    withCredentials: false
+  })
+    .then(function (res) {
+      success(res.data)
+    })
+    .catch(function (err) {
+      if (err) {
+        console.log('api error? : ' + err);
+      }
+    })
+}
+
+// 返回在vue模板中的调用接口
+export default {
+  get: function (url, params, success, failure) {
+    return apiAxios('GET', url, params, success, failure)
+  },
+  post: function (url, params, success, failure) {
+    return apiAxios('POST', url, params, success, failure)
+  },
+  put: function (url, params, success, failure) {
+    return apiAxios('PUT', url, params, success, failure)
+  },
+  delete: function (url, params, success, failure) {
+    return apiAxios('DELETE', url, params, success, failure)
+  }
+}
---
+++ webapp/src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '@/page/index'
+// import PoetCatalog from '@/page/poetcatalog'
+// import PoetRelation from '@/page/poetrelation'
+// import PoetWay from '@/page/poetway'
+// import PoetCard from '@/page/poetcard'
+// import PoetInfo from '@/page/poetinfo'
+// import PoemInfo from '@/page/poeminfo'
+// import PoemCard from '@/page/poemcard'
+// import SearchAll from '@/page/searchall'
+// import SearchModel from '@/page/searchmodel'
+// import Introduce from '@/page/introduce'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: Index
+    // }, {
+    //   path: '/poetcatalog',
+    //   component: PoetCatalog
+    // },{
+    //   path: '/poetrelation',
+    //   component: PoetRelation
+    // },{
+    //   path: '/poetcard',
+    //   component: PoetCard
+    // },{
+    //   path: '/poetinfo/:id',
+    //   component: PoetInfo
+    // },{
+    //   path: '/poetway',
+    //   component: PoetWay
+    // },{
+    //   path: '/poemcard',
+    //   component: PoemCard
+    // },{
+    //   path: '/poeminfo/:id',
+    //   component: PoemInfo
+    // },{
+    //   path: '/searchall',
+    //   component: SearchAll
+    // },{
+    //   path: '/searchmodel',
+    //   component: SearchModel
+    // },{
+    //   path: '/introduce',
+    //   component: Introduce
+    }
+  ]
+})
